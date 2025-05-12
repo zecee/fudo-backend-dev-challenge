20974d63f4b7d4429869921eecefb086ac66de7e
Zece <zece.dev@proton.me>
2025-05-12 00:50:31 -0300

    feat(api): Declare and expose API version in responses

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
-API_V1_NAMESPACE = '/api/v1'.freeze
+API_VERSION = 'v1'.freeze
+API_V1_NAMESPACE = "/api/#{API_VERSION}".freeze
 
 require 'sinatra'
 require 'securerandom'
@@ -8,6 +9,11 @@ require_relative "./routes#{API_V1_NAMESPACE}/user_routes"
 require_relative "./routes#{API_V1_NAMESPACE}/product_routes"
 
 enable :sessions
+
+after do
+  headers 'X-API-Version' => API_VERSION if request.path_info.start_with?(API_V1_NAMESPACE)
+end
+
 set :session_secret, SecureRandom.hex(64)
 
 before { content_type 'application/json' }
